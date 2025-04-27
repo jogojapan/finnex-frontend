882751b700ec7865062fd7299bc4ac3156bfa787
Johannes Goller <j@jogojapan.xyz>
2025-04-27 20:02:49 +0200

    Indentation.

--- src/App.tsx
+++ src/App.tsx
@@ -5,48 +5,47 @@ import Login from './Login';
 import Main from './Main';
 import NewAccount from './NewAccount';
 import AccountList from './AccountList';
+import Layout from './Layout';
 
 const App: React.FC = () => {
  const [token, setToken] = useState(localStorage.getItem('token'));
 
  useEffect(() => {
- if (token) {
- axios.defaults.headers.common['Authorization'] = `Token ${token}`;
- } else {
- delete axios.defaults.headers.common['Authorization'];
- }
+   if (token) {
+     axios.defaults.headers.common['Authorization'] = `Token ${token}`;
+   } else {
+     delete axios.defaults.headers.common['Authorization'];
+   }
  }, [token]);
 
- const handleLoginSuccess = (newToken: string) => {
- setToken(newToken);
- window.location.replace('/');
- };
+  const handleLoginSuccess = (newToken: string) => {
+    setToken(newToken);
+    window.location.replace('/');
+  };
 
- const handleLogout = () => {
- setToken(null);
- };
+  const handleLogout = () => {
+    setToken(null);
+  };
 
  return (
- <BrowserRouter>
- <Routes>
- <Route
- path="/login"
- element={
- token ? (
- <Navigate to="/" replace />
- ) : (
- <Login onLoginSuccess={handleLoginSuccess} />
- )
- }
- />
- <Route
- path="/"
- element={token ? <Main onLogout={handleLogout} /> : <Navigate to="/login" replace />}
- />
- <Route path='/accounts/new' element={token ? <NewAccount /> : <Navigate to="/login" replace />} />
- <Route path='/accounts/list' element={token ? <AccountList /> : <Navigate to="/login" replace />} />
- </Routes>
- </BrowserRouter>
+   <BrowserRouter>
+     <Routes>
+     <Route element={<Layout />}>
+       <Route path="/login" element={
+         token ? (
+           <Navigate to="/" replace />
+         ) : (
+           <Login onLoginSuccess={handleLoginSuccess} />
+         )
+       }/>
+       <Route path="/" element={
+         token ? <Main onLogout={handleLogout} /> : <Navigate to="/login" replace />
+       }/>
+       <Route path='/accounts/new' element={token ? <NewAccount /> : <Navigate to="/login" replace />} />
+       <Route path='/accounts/list' element={token ? <AccountList /> : <Navigate to="/login" replace />} />
+     </Route>
+     </Routes>
+   </BrowserRouter>
  );
 };
 
